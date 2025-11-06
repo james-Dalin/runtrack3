0e5c2ba6c7a51120a3fc1211189d4dfa2150c716
james.Dalin <james.dalin@laplateforme.io>
2025-11-06 11:40:24 +0100

    Ajout du commentaire pour dire ce que fait le console.log

--- Job3/script.js
+++ Job3/script.js
@@ -4,4 +4,6 @@
  * Modifiez vos fichiers de sorte à ce que l’affichage de la popup se fasse maintenant dans le fichier
 script.js.*/
 
+//On utilise le console.log pour afficher le message dans la console
+
 console.log("Tout fonctionne. Hello Javascript !");
