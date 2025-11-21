4c5947c5014c8108b7ba24d05c1142b9804d1972
james.Dalin <james.dalin@laplateforme.io>
2025-11-21 15:58:59 +0100

    Job1 du Jour3 fait

--- Jour3/Job1/script.js
+++ Jour3/Job1/script.js
@@ -1,13 +1,11 @@
 $(document).ready(function() {
     // Je créer le bouton pour montrer
-    $("#btnShow").click(function(){
+    $("#btnShow").click(function() {
         $("#citation").text("Les logiciels et les cathédrales, c'est un peu la même chose - d'abord on les construit, ensuite on prie.").show();
     });
     // Je créer le bouton pour cacher
-    $("#btnHide").click(function(){
-        $("#btnHide").click(function(){
-            $("#citation").hide();
-        });
-    });
+    $("#btnHide").click(function() {
+        $("#citation").hide();
+    })
 });
 
