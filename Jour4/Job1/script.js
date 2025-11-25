8235a974bdded812858bc08ab00d4dee33e25747
james.Dalin <james.dalin@laplateforme.io>
2025-11-25 13:40:14 +0100

    Job1 du JOUR4 fait

--- Jour4/Job1/script.js
+++ Jour4/Job1/script.js
@@ -1,1 +1,15 @@
-fetch("expression.txt")
+function afficherExpression() {
+    fetch("expression.txt") // Lance une requête pour récuéprer le fichier
+        .then(reponse => reponse.text())
+        .then(contenu => {
+            let paragraphe = document.createElement("p");
+            paragraphe.textContent = contenu;
+            document.body.appendChild(paragraphe);
+        })
+    .catch(erreur => {
+        console.log("Erreur:", erreur);
+    })
+}
+
+let button = document.querySelector("#button");
+button.addEventListener("click", afficherExpression);
