bbe9a430ceb7175146ddf832e4a375d46d35e58e
james.Dalin <james.dalin@laplateforme.io>
2025-11-25 14:09:40 +0100

    Modification pour qu'un seul message soit affiché

--- Jour4/Job1/script.js
+++ Jour4/Job1/script.js
@@ -1,14 +1,20 @@
 function afficherExpression() {
-    fetch("expression.txt") // Lance une requête pour récuéprer le fichier
+    let paragrapheExiste = document.querySelector("#expression");
+    if (paragrapheExiste) {
+        return;
+    }
+
+    fetch("expression.txt") // Lance une requête pour récuéprer le fichier
         .then(reponse => reponse.text())
         .then(contenu => {
             let paragraphe = document.createElement("p");
+            paragraphe.id = "expression";
             paragraphe.textContent = contenu;
             document.body.appendChild(paragraphe);
         })
     .catch(erreur => {
-        console.log("Erreur:", erreur);
-    })
+        console.error("Erreur:", erreur);
+    });
 }
 
 let button = document.querySelector("#button");
