3704ae7a35b6299b65114b63a025edb209216f1b
james.Dalin <james.dalin@laplateforme.io>
2025-11-26 09:44:28 +0100

    Job2 du Jour4 fait

--- Jour4/Job2/script.js
+++ Jour4/Job2/script.js
@@ -31,3 +31,6 @@ function jsonValueKey(json, key) {
 let jsonString = '{"name" : "La Plateforme_", "address": "8 rue d\`hozier", "city": "Marseille", "nb_staff": "11", "creation": "2019"}';
 
 console.log(jsonValueKey(jsonString, "city"));
+console.log(jsonValueKey(jsonString, "name"));
+console.log(jsonValueKey(jsonString, "address"));
+console.log(jsonValueKey(jsonString, "nb_staff"));
