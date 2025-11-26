265033e1980be22ae1d6df73c9dcaa2aad5c1420
james.Dalin <james.dalin@laplateforme.io>
2025-11-26 11:09:08 +0100

    petite correction de l'erreru backtick

--- Jour4/Job2/script.js
+++ Jour4/Job2/script.js
@@ -1,34 +1,14 @@
-// /**
-//  * Convertir une string JSON en objet JS : entrée et sortie => string à objet
-//  */
-// let texteJSON = '{"name": "LaPlateforme_", "city": "Marseille"}';
-// let objet = JSON.parse(texteJSON);
-
-
-// // APRES JSON parse()
-// console.log(typeof objet);
-// console.log(objet.city);
-
-// /**
-//  * Convertir un objet JS en string JSON : entré et sortie => objet à string ==> JSON.stringify
-//  */
-// let objet1 = {name: "La Plateforme", city: "Marseille" };
-// let texteJSON1 = JSON.stringify(objet);
-
-// // AVANT le JSON.stringify
-// console.log(typeof objet);
-// console.log(objet);
-
-// // APRES le JSON.stringify
-// console.log(typeof JSON);
-// console.log(texteJSON);
-
 function jsonValueKey(json, key) {
-    let objet = JSON.parse(json);
-    return objet[key];
+    try{
+        let objet = JSON.parse(json);
+        return objet[key];
+    } catch (erreur){
+        console.error("Erreur :", erreur);
+        return null;
+    }
 }
 
-let jsonString = '{"name" : "La Plateforme_", "address": "8 rue d\`hozier", "city": "Marseille", "nb_staff": "11", "creation": "2019"}';
+let jsonString = '{"name" : "La Plateforme_", "address": "8 rue d\'hozier", "city": "Marseille", "nb_staff": "11", "creation": "2019"}';
 
 console.log(jsonValueKey(jsonString, "city"));
 console.log(jsonValueKey(jsonString, "name"));
