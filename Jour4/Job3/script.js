ce3522b53349ccc7af44c0fe21fff3f60dc94590
james.Dalin <james.dalin@laplateforme.io>
2025-11-27 17:00:45 +0100

    Job3 JOUR4 en cours

--- Jour4/Job3/script.js
+++ Jour4/Job3/script.js
@@ -12,6 +12,29 @@ window.addEventListener("load", function() {
     .catch (erreur => console.error(erreur));
 });
 
+// Au clic du bouton
 document.getElementById("btnFiltrer").addEventListener("click", function()) {
-
+    let resultats = filtrer???(???);
+    afficher???(resultats);
 };
+
+// Fonction : remplir la select avec le type
+function remplir???(pokémons) {
+    let types = pokémons.map(pokemon => pokemon.???);
+    let typesUniques = new ??? types);
+
+    let select = document.getElementById("inputType");
+    typesUniques.forEach(type => {
+        let option = document.createElement("???");
+        option.value = type;
+        option.textContent = type;
+        select.appendChild(option);
+    });
+}
+
+// Fonction : Filtrer les Pokémons
+function filtrer???(pokémons) {
+    let id = document.getElementById("inputId").???;
+    let nom = document.getElementById("inputNom").???;
+    let type = document.getElementById("inputType").???;
+}
