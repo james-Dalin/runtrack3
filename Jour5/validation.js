0197c87fb7d988051321a8b4bdb3926cbc7955fa
drajoan258 <charlestedison@gmail.com>
2025-12-14 23:19:58 +0100

    validation.js fait

--- Jour5/validation.js
+++ Jour5/validation.js
@@ -13,3 +13,17 @@ function hasPasswordStrength(password) {
   return hasUpperCase && hasLowerCase && hasNumbers;
 }
 
+function getPasswordStrength(password) {
+  if (password.length < 8) return 'weak';
+
+  const hasUpperCase = /[A-Z]/.test(password);
+  const hasLowerCase = /[a-z]/.test(password);
+  const hasNumbers = /\d/.test(password);
+  const hasSpecialChar = /[!@#$%^&*]/.test(password);
+
+  const strength = [hasUpperCase, hasLowerCase, hasNumbers, hasSpecialChar].filter(Boolean).length;
+
+  if (strength <= 2) return 'weak';
+  if (strength <= 3) return 'medium';
+  return 'strong';
+}
