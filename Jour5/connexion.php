d27ba95004f2e6a9e88e4e9de04187d651477498
drajoan258 <charlestedison@gmail.com>
2025-12-14 22:30:27 +0100

    fichier de connexion en cours N°1

---
+++ Jour5/connexion.php
@@ -0,0 +1,22 @@
+<?php 
+
+require 'config.php';
+
+// Si déjà connecté, redirige vers l'accueil
+if (isLoggedIn()) {
+  header('Location: index.php');
+  exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Connexion</title>
+</head>
+<body>
+  
+</body>
+</html>
